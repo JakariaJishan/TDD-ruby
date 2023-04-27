7fb4d97941a0f7119b94140392b0eab0818198d2
Vanessa O <vaneolipa@gmail.com>
2023-04-27 11:37:55 -0500

    Create reserve and fizzbuzz methods

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 class Solver
-    def factorial(N)
-        
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.negative?
+      raise ArgumentError, 'Negative are not allowed'
+    else
+      factorial(num-1) * num
     end
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    else 
+      num.to_s
+    end
+  end
 end
